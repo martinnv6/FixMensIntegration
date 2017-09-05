2f68d312eb2390b37fc7409a2846e3bccc4e8a58
Martin Navarrete Villegas <martin.navarrete@neoris.com>
2017-09-04 19:29:23 -0500

    se agrego insights y se agrego nuevos campos a REPARACIONESVIEW, ademas se agregaron dos metodos de lectura y actualizacion de tabla REPARACIONES_HISTORICO_ESTATUS para obtener los cambios recientes no integrados.

--- FixmensCMD/BLL/OrdenBLL.cs
+++ FixmensCMD/BLL/OrdenBLL.cs
@@ -13,32 +13,88 @@ namespace FixmensCMD.BLL
 {
     public class OrdenBLL
     {
-        public List<REPARACIONESVIEW> GetReparaciones()
+        public List<long> GetStatusChanged()
         {
+            var result = new List<long>();
 
-            var result = new List<REPARACIONESVIEW>(); 
-            
-            
-            
+
+
             FbConnection conn = new FbConnection();
             FbCommand cmd = new FbCommand();
             FbDataAdapter da = new FbDataAdapter();
             DataTable dt = new DataTable();
             string firebirdServer = ConfigurationManager.AppSettings["firebird.server"];
-            
+
             conn.ConnectionString = "User=SYSDBA;Password=masterkey;Database=" + firebirdServer +
                                     ";Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
                                     "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
             cmd.Connection = conn;
-            
+
             string stardate = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
-            
-            
-            cmd.CommandText = "SELECT R.CODIGO, a.MARCA || ' - ' || a.MODELO as EQUIPO, C.NOMBRES || ' ' || C.telefono || ' ' || C.celular as NOMBRES,R.FALLA,R.INFORMETALLER,P.DETALLE, P.PRESUPUESTO, I.NOMBRES AS TECNICO, R.FECHAINGRESO,R.PROMETIDO,R.FECHATERMINADO, ESTADO.NOMBRE ESTADO " +
+
+
+
+
+            cmd.CommandText = "SELECT rh.IDREPARACION from REPARACIONES_HISTORICO_ESTADO rh " +
+                                "WHERE RH.INTEGRADO = 'N' " +
+                                "group by rh.IDREPARACION";
+            cmd.CommandType = CommandType.Text;
+
+
+            conn.Open();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            conn.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    result.Add(int.Parse(row["IDREPARACION"].ToString()));
+                }
+
+
+            }
+
+
+
+            return result;
+
+        }
+
+
+        public List<REPARACIONESVIEW> GetReparaciones()
+        {
+
+            var result = new List<REPARACIONESVIEW>();
+
+
+
+            FbConnection conn = new FbConnection();
+            FbCommand cmd = new FbCommand();
+            FbDataAdapter da = new FbDataAdapter();
+            DataTable dt = new DataTable();
+            string firebirdServer = ConfigurationManager.AppSettings["firebird.server"];
+
+            conn.ConnectionString = "User=SYSDBA;Password=masterkey;Database=" + firebirdServer +
+                                    ";Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
+                                    "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            cmd.Connection = conn;
+
+            string stardate = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
+
+
+
+
+            cmd.CommandText = "SELECT R.CODIGO, a.MARCA || ' - ' || a.MODELO as EQUIPO, C.NOMBRES  as NOMBRES, " +
+                              "C.telefono AS TELEFONO, C.celular as CELULAR, C.EMAIL AS EMAIL,R.FALLA,R.INFORMETALLER,P.DETALLE, " +
+                              "P.PRESUPUESTO, I.NOMBRES AS TECNICO, R.FECHAINGRESO,R.PROMETIDO,R.FECHATERMINADO, ESTADO.NOMBRE ESTADO, " +
+                              "R.ENTREGADO, R.FECHAENTREGADO, R.NS, R.AVISADO " +
                               "FROM REPARACIONES R JOIN CLIENTES C ON R.CLIENTE = C.CODIGO " +
                               "JOIN PRESUPUESTOS P ON R.CODIGO = P.IDREPARACION " +
                               "JOIN INTEGRANTES I ON R.TECNICO = I.CODIGO " +
                               "inner join ESTADO on estado.CODIGO = R.ESTADO " +
+                              "inner join APARATO a on R.NS = a.NS " +
                               "inner join APARATO a on R.NS = a.NS " +
                               "WHERE R.FECHA_AUD > '" + stardate + "' AND R.CODIGO > 10000";
             cmd.CommandType = CommandType.Text;
@@ -73,17 +129,53 @@ namespace FixmensCMD.BLL
                         FECHAUPDATE = DateTime.Now
                     };
                     result.Add(rowResult);
-                   }
-                
+                }
+
 
             }
             else
             {
-                
+
             }
 
 
             return result;
+        }
+
+        public void UpdateStatusChanged(List<long> ordersId)
+        {
+           
+
+
+
+            FbConnection conn = new FbConnection();
+            FbCommand cmd = new FbCommand();
+            FbDataAdapter da = new FbDataAdapter();
+            DataTable dt = new DataTable();
+            string firebirdServer = ConfigurationManager.AppSettings["firebird.server"];
+
+            conn.ConnectionString = "User=SYSDBA;Password=masterkey;Database=" + firebirdServer +
+                                    ";Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
+                                    "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            cmd.Connection = conn;
+
+            string orders = string.Join<long>(",", ordersId);
+
+
+
+
+            cmd.CommandText = "update REPARACIONES_HISTORICO_ESTADO rh " +
+                              "set RH.INTEGRADO = 'Y' " +
+                              "where rh.IDREPARACION in ("+ orders + ")";
+            cmd.CommandType = CommandType.Text;
+
+
+            conn.Open();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            conn.Close();
+
+            
         }
     }
 }
--- FixmensCMD/Program.cs
+++ FixmensCMD/Program.cs
@@ -20,17 +20,24 @@ namespace FixmensCMD
             Console.Write("\n\n Leyendo cambios recientes en ordenes.....");
             OrdenBLL bllOrden = new OrdenBLL();
             var res = bllOrden.GetReparaciones();
-            
+            var changed = bllOrden.GetStatusChanged();
+
             FixMensSQLModel modelSql = new FixMensSQLModel();
+
             
             foreach (var item in res)
             {
-                
+                if (changed.Contains(item.CODIGO)) { 
                 modelSql.REPARACIONESVIEWs.AddOrUpdate(x=> x.CODIGO, item);
                 Console.Write("\n Orden actualizada :" + item.CODIGO);
+                }
             }
             Console.Write("\n Integrando datos a Microsoft AZURE...");
             modelSql.SaveChanges();
+            
+            //Update to integrated orders
+            bllOrden.UpdateStatusChanged(changed);
+
             Console.Write("\n Ordenes se actualizaron correctamente ......Presione una tecla");
 
             //Console.ReadKey();
